fd7c11a076c1b1ca9e6cca559b539d482aef6def
SangeethaShanmugm <sanjuriya22@gmail.com>
2024-02-08 20:43:00 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import SideBar from './component/SideBar';
+import TopBar from './component/TopBar';
+import Dashboard from './component/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div id="wrapper">
+          <SideBar />
+          <div id="content-wrapper" className="d-flex flex-column">
+            <div id="content">
+              <TopBar />
+              <div className="container-fluid">
+                <Switch>
+                  <Route path="/" exact={true} component={Dashboard} />
+                </Switch>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+
+      </div>
+    </BrowserRouter>
   );
 }
 
